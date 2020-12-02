491ea5eb5600d6da02b7753331a6ff0848064ba8
James Glenn <jglenn1@gmail.com>
2020-12-01 20:37:51 -0600

    day 1 initial solution

---
+++ day-1/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func getInput() []string {
+	data, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.Fields(string(data))
+}
+
+func part1Iteration(entries []string) int {
+	for i := 0; i < len(entries); i++ {
+		for j := 0; j < len(entries); j++ {
+			if i == j {
+				continue
+			}
+
+			outerEntry, _ := strconv.Atoi(entries[i])
+			innerEntry, _ := strconv.Atoi(entries[j])
+			if innerEntry+outerEntry == 2020 {
+				return innerEntry * outerEntry
+			}
+		}
+	}
+
+	return -1
+}
+
+func part2Iteration(entries []string) int {
+	for i := 0; i < len(entries); i++ {
+		for j := 0; j < len(entries); j++ {
+			for k := 0; k < len(entries); k++ {
+				if i == j || j == k || i == k {
+					continue
+				}
+
+				outerEntry, _ := strconv.Atoi(entries[i])
+				midEntry, _ := strconv.Atoi(entries[j])
+				innerEntry, _ := strconv.Atoi(entries[k])
+				if innerEntry+midEntry+outerEntry == 2020 {
+					return innerEntry * midEntry * outerEntry
+				}
+			}
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	entries := getInput()
+	fmt.Println(part1Iteration(entries))
+	fmt.Println(part2Iteration(entries))
+}
