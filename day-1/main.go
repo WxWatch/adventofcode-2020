7656290f8e1ae1f1caf0b27c067f849543f485d1
James Glenn <jglenn1@gmail.com>
2020-12-01 21:07:15 -0600

    timing and other random stuff

--- day-1/main.go
+++ day-1/main.go
@@ -4,29 +4,45 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
-func getInput() []string {
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	log.Printf("%s took %s", name, elapsed)
+}
+
+func getInput() []int {
 	data, err := ioutil.ReadFile("input")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return strings.Fields(string(data))
+	stringEntries := strings.Fields(string(data))
+	entries := make([]int, 0)
+	for _, entry := range stringEntries {
+		entryInt, _ := strconv.Atoi(entry)
+		entries = append(entries, entryInt)
+	}
+	return entries
 }
 
 /* Naive solution; gets worse the bigger the initial entries array is */
-func part1Iteration(entries []string) int {
+func part1Iteration(entries []int) int {
+	defer timeTrack(time.Now(), "part1Iteration")
+	sort.Ints(entries)
+
 	for i := 0; i < len(entries); i++ {
 		for j := 0; j < len(entries); j++ {
 			if i == j {
 				continue
 			}
 
-			outerEntry, _ := strconv.Atoi(entries[i])
-			innerEntry, _ := strconv.Atoi(entries[j])
+			outerEntry := entries[i]
+			innerEntry := entries[j]
 			if innerEntry+outerEntry == 2020 {
 				return innerEntry * outerEntry
 			}
@@ -36,18 +52,40 @@ func part1Iteration(entries []string) int {
 	return -1
 }
 
+/* Slower than part1Iteration */
+func part1SortBinaryIteration(entries []int) int {
+	defer timeTrack(time.Now(), "part1Sort")
+	sort.Ints(entries)
+
+	for _, entry := range entries {
+		i := sort.Search(len(entries), func(i int) bool {
+			return entry+entries[i] >= 2020
+		})
+		if i < len(entries) && entry+entries[i] == 2020 {
+			// x is present at data[i]
+			return entry * entries[i]
+		}
+	}
+
+	return -1
+}
+
 /* Naive solution; gets worse the bigger the initial entries array is */
-func part2Iteration(entries []string) int {
+func part2Iteration(entries []int) int {
+	defer timeTrack(time.Now(), "part2Iteration")
+	sort.Ints(entries)
+
 	for i := 0; i < len(entries); i++ {
 		for j := 0; j < len(entries); j++ {
 			for k := 0; k < len(entries); k++ {
+
 				if i == j || j == k || i == k {
 					continue
 				}
 
-				outerEntry, _ := strconv.Atoi(entries[i])
-				midEntry, _ := strconv.Atoi(entries[j])
-				innerEntry, _ := strconv.Atoi(entries[k])
+				outerEntry := entries[i]
+				midEntry := entries[j]
+				innerEntry := entries[k]
 				if innerEntry+midEntry+outerEntry == 2020 {
 					return innerEntry * midEntry * outerEntry
 				}
@@ -59,7 +97,15 @@ func part2Iteration(entries []string) int {
 }
 
 func main() {
+	/* Notes:
+	 * I sort first in every solution to get a more accurate indication of how long each function takes
+	 */
 	entries := getInput()
 	fmt.Println(part1Iteration(entries))
+
+	entries = getInput()
 	fmt.Println(part2Iteration(entries))
+
+	entries = getInput()
+	fmt.Println(part1SortBinaryIteration(entries))
 }
